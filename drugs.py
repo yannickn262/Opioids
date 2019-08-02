f0b7dcaaa90052a39d374c09e6d1466ce3497219
nandury <nandury@umich.edu>
2019-08-02 16:14:02 -0400

    Disappointed

--- drugs.py
+++ drugs.py
@@ -7,15 +7,63 @@ from numpy import *
 from matplotlib.pyplot import *
 from pylab import *
 from scipy.optimize import curve_fit
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
 import scipy.optimize as opt
+import dateutil
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+
+def f(x):
+    # function to approximate by polynomial interpolation
+    return x * np.sin(x)
+
+#Work in Progress: Attempting to use sklearn to predict ages
+def predict_ages():
+    # generate points used to plot
+    x_plot = np.linspace(0, 10, 100)
+
+    # generate points and keep a subset of them
+    x = np.linspace(0, 10, 100)
+    rng = np.random.RandomState(0)
+    rng.shuffle(x)
+    x = np.sort(x[:20])
+    y = f(x)
+
+    # create matrix versions of these arrays
+    X = x[:, np.newaxis]
+    X_plot = x_plot[:, np.newaxis]
+
+    colors = ['teal', 'yellowgreen', 'gold']
+    lw = 2
+    plt.plot(x_plot, f(x_plot), color='cornflowerblue', linewidth=lw,
+         label="ground truth")
+    plt.scatter(x, y, color='navy', s=30, marker='o', label="training points")
+
+    for count, degree in enumerate([3, 4, 5]):
+        model = make_pipeline(PolynomialFeatures(degree), Ridge())
+        model.fit(X, y)
+        y_plot = model.predict(X_plot)
+        plt.plot(x_plot, y_plot, color=colors[count], linewidth=lw,
+             label="degree %d" % degree)
+
+    plt.legend(loc='lower left')
+    plt.show()
+
+
+
+
+#curve_fit paramaters
+#scipy curve fitting techniques
+#train model - split 50,000 and then 20,000
+# Create Predictive model - scikit
 
 
 data = pd.read_csv('CTDrugs.csv')
 total_count = data.shape[0]
-
-# This is the function we are trying to fit to the data. Used for Fitted Curve
-def func(x, a, b, c):
-     return a * np.exp(-b * x) + c
 
 
 def displayGender():
@@ -24,10 +72,14 @@ def displayGender():
     colors = ['turquoise', 'pink']
     gender_counts.plot(kind = 'bar', color = colors)
     num_cols = ["Male", "Female", "Other"]
-    plt.title("Deaths by Sex")
+    plt.title("Deaths by Gender")
     plt.xlabel("Sex")
     plt.ylabel("Percentage %")
     plt.show()
+
+# This is the function we are trying to fit to the data. Used for Fitted Curve
+def func(x, a, b, c):
+     return a * np.exp(-b * x) + c
 
 def displayAges():
     ages_counts = data['Age'].value_counts(normalize = True)
@@ -40,16 +92,20 @@ def displayAges():
     plt.title("Deaths by Age")
     plt.xlabel("Age")
     plt.ylabel("Percentage %")
-
+    xp = linspace(10,90, 100)
     # calculate polynomial for fitted curve
     z = np.polyfit(x, y, 3)
     f = np.poly1d(z)
 
+    p2 = np.polyfit(x, y, 11)
+    plot(xp, polyval(p2, xp), 'b--')
+
     # calculate new x's and y's for fitted curve
     x_new = np.linspace(x[0], x[-1], 50)
     y_new = f(x_new)
+
     #plot fitted regression curve
-    plt.plot(x,y,'o', x_new, y_new)
+    #plt.plot(x,y,'o', x_new, y_new)
 
     # Optional: calculate trendline - Uncomment if you want linear
     # z = np.polyfit(x, y, 1)
@@ -65,28 +121,19 @@ def displayRace():
     race_count.plot(kind = 'barh', title = "Opioid Deaths by Ethnicity")
     plt.xlabel("Death Percentage %")
 
-
-#Future Implementation: Want to calculate what seasons have the most overdoses
-def displayMonth():
-    date_count = data['Date'].value_counts(normalize = True)
-    date_count = date_count * 100
-    # for day in data:
-    #     datestring = data['Date']
-    #     splitDate = datestring.str.split("/")
-    #     month = splitDate
-    #     print(month)
-
-
 def drug_percentage():
     heroin_log = {}
     count = 0
     heroin_y_count = 0
     cocaine_y_count = 0
     fentanyl_y_count = 0
+    alcohol_y_count = 0
+
     for row in data:
         heroin_yes = data['Heroin']
         cocaine_yes = data['Cocaine']
         fentanyl_yes = data['Fentanyl']
+        alcohol_yes = data['Ethanol']
 
     for row in heroin_yes:
         if (row == "Y"):
@@ -97,16 +144,53 @@ def drug_percentage():
     for row in fentanyl_yes:
         if (row == "Y"):
             fentanyl_y_count += 1
+    for row in alcohol_yes:
+        if (row == "Y"):
+            alcohol_y_count += 1
 
     percentage_heroin = (heroin_y_count/total_count) * 100
     percentage_cocaine = (cocaine_y_count/total_count) * 100
     percentage_fentanyl = (fentanyl_y_count/total_count) * 100
+    percentage_alcohol = (alcohol_y_count/total_count) * 100
 
-    #update by implementing string formatting to make universal for various datasets
+
     print("Proportion of Opioid Deaths caused by Heroin: ", percentage_heroin, "%")
     print("Proportion of Opioid Deaths caused by Cocaine: ", percentage_cocaine, "%")
     print("Proportion of Opioid Deaths caused by Fentanyl: ", percentage_fentanyl, "%")
+    print("Proportion of Opioid Deaths caused by Alcohol: ", percentage_alcohol, "%")
 
+
+#Future Implementation: Display Drug Percentages Visually
+
+#Future Implementation: Want to calculate what seasons have the most overdoses
+def displayMonth():
+    date_count = data['Date'].value_counts(normalize = True)
+    date_count = date_count * 100
+
+    data['Date'] = pd.to_datetime(data['Date'])
+    dg = data.groupby([data['Date'].dt.year.rename('year'), data['Date'].dt.month.rename('month')]).agg({'count'})
+    #print(dg)
+
+    # months = {}
+    # for year in dg:
+    #     month = dg[1]
+    #     month_count = dg[2]
+    #     if month in months:
+    #         months[month] += month_count
+
+    #print(months)
+
+    #dg.plot(kind = 'barh', title = "Deaths by Season")
+
+    # for day in data:
+    #     datestring = data['Date']
+    #     splitDate = datestring.str.split("/")
+    #     month = splitDate
+    #     print(month)
+
+
+
+predict_ages()
 displayGender()
 displayAges()
 displayRace()
